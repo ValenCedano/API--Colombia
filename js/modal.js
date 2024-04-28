884f7d8013bc94a78b038aef21a8d4f34ec56f9f
Valentina Cedano <valentina.cedano57@gmail.com>
2024-04-28 01:24:27 -0500

    Realizando modo oscuro/claro

--- js/modal.js
+++ js/modal.js
@@ -4,3 +4,44 @@ const toggleModal = (button, modal) => {
         modal.classList.toggle("hidden");
     });
 };
+
+const toggleColor = (button,body,imagen) =>{
+    let dark = true;
+    button.addEventListener("click", () =>{
+        dark = !dark; 
+
+        body.classList.toggle("dark");
+
+        if (!dark) {
+            button.src = "assets/icons/moon-icon.svg"; 
+            imagen.src = "assets/logo-dark.svg"
+            
+        } else {
+            button.src = "assets/icons/sun-icon.svg";
+            imagen.src = "assets/logo-light.svg"
+        }
+        
+    });
+};
+
+const toggleColor2 = (button,body,imagen,cuerpo,imagen2) =>{
+    let dark = true;
+    button.addEventListener("change", () =>{
+        dark = !dark; 
+        cuerpo.classList.toggle("dark")
+        body.classList.toggle("dark");
+
+        if (!dark) { 
+            imagen.src = "assets/icons/moon-icon.svg"
+            imagen2.src= "assets/logo-dark.svg"
+            
+        } else {
+            imagen.src = "assets/icons/sun-icon.svg"
+            imagen2.src = "assets/logo-light.svg"
+        }
+        
+    });
+};
+
+
+
--- js/scriptHome.js
+++ js/scriptHome.js
@@ -1,7 +1,22 @@
 const botonMenu = document.getElementById("hamburguer");
 const modal = document.querySelector(".modal");
 const closeButton = document.getElementById("closeModal");
+const botonColor = document.getElementById("modoWeb");
+const bodyContent = document.body;
+const modale= document.getElementsByClassName("contenidoModal");
+const imagencambia = document.getElementById("Logo-API");
+const selectTheme = document.getElementById("select-theme");
+const mododod = document.getElementById("modoWeb2")
+
+console.log(bodyContent);
+console.log(botonColor);
+console.log(imagencambia);
+console.log(modale)
 toggleModal(botonMenu,modal);
 toggleModal(closeButton,modal);
+
+toggleColor(botonColor,bodyContent,imagencambia);
+toggleColor2(selectTheme,modale[0],mododod,bodyContent,imagencambia);
+
 
 
